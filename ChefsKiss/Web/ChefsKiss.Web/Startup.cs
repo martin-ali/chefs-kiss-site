0098d7d495b643aebdfe41934d4dcb48ca433071
Martin <marto7395@gmail.com>
2021-06-27 21:13:05 +0300

    Updated project to .NET Core 5.0

--- ChefsKiss/Web/ChefsKiss.Web/Startup.cs
+++ ChefsKiss/Web/ChefsKiss.Web/Startup.cs
@@ -30,10 +30,11 @@ namespace ChefsKiss
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,7 @@ namespace ChefsKiss
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {

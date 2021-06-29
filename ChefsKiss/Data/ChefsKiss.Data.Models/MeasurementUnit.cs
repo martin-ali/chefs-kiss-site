8fcdce0e4a43ce52e78831d4219e415f4413939e
Martin <marto7395@gmail.com>
2021-06-29 12:06:24 +0300

    Added MeasurementUnit enum

---
+++ ChefsKiss/Data/ChefsKiss.Data.Models/MeasurementUnit.cs
@@ -0,0 +1,11 @@
+namespace ChefsKiss.Data.Models
+{
+    public enum MeasurementUnit
+    {
+        Micrograms,
+        Milligrams,
+        Grams,
+        Kilograms,
+        Teaspoons,
+    }
+}

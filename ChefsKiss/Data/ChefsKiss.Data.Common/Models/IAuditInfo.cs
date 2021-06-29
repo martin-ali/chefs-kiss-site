b1c76a7cfc20a60910141572ded50572d87382b3
Martin <marto7395@gmail.com>
2021-06-29 10:14:59 +0300

    Added common BaseModel and IAuditInfo

---
+++ ChefsKiss/Data/ChefsKiss.Data.Common/Models/IAuditInfo.cs
@@ -0,0 +1,11 @@
+namespace ChefsKiss.Data.Common.Models
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}

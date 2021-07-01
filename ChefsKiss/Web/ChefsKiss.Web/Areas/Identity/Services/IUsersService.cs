25bf772b28635f489eaf5083eed7a8c613fd7904
Martin <marto7395@gmail.com>
2021-07-01 21:58:21 +0300

    Stopped work on Identity for the moment, reverted changes

--- ChefsKiss/Web/ChefsKiss.Web/Areas/Identity/Services/IUsersService.cs
+++
@@ -1,9 +0,0 @@
-namespace ChefsKiss.Web.Areas.Identity.Services
-{
-    using ChefsKiss.Web.Areas.Identity.ViewModels;
-
-    public interface IUsersService
-    {
-        UserDetailsViewModel GetProfileById(string id);
-    }
-}

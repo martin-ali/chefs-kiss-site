c132e76723dcefa2650d1f68603944576bd2c6c7
Martin <marto7395@gmail.com>
2021-07-03 11:39:37 +0300

    Added annotations to CommentCreateFormModel for validation

--- ChefsKiss/Web/ChefsKiss.Web/Areas/Recipes/ViewModels/Comments/CommentCreateFormModel.cs
+++ ChefsKiss/Web/ChefsKiss.Web/Areas/Recipes/ViewModels/Comments/CommentCreateFormModel.cs
@@ -1,9 +1,14 @@
 namespace ChefsKiss.Web.Areas.Recipes.ViewModels.Comments
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CommentCreateFormModel
     {
         public int RecipeId { get; init; }
 
+        [Required]
+        [MinLength(10)]
+        [MaxLength(500)]
         public string Content { get; init; }
     }
 }

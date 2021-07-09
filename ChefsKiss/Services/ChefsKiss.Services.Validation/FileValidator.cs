849d0722435cf48af3f25b3ac9bcad26e04fcf81
Martin <marto7395@gmail.com>
2021-07-09 20:10:07 +0300

    Corrected FileValidator to work with redesigned AllowedImageExtensions const

--- ChefsKiss/Services/ChefsKiss.Services.Validation/FileValidator.cs
+++ ChefsKiss/Services/ChefsKiss.Services.Validation/FileValidator.cs
@@ -3,15 +3,17 @@ namespace ChefsKiss.Services.Validation
     using System.IO;
     using System.Linq;
 
-    using static ChefsKiss.Common.GlobalConstants;
+    using static ChefsKiss.Common.DataConstants;
 
     public class FileValidator : IFileValidator
     {
+        private const char ExtensionSeparator = ',';
+
         public void ThrowIfExtensionIsInvalid(string extension)
         {
-            if (AllowedImageExtensions.Any(x => extension.EndsWith(x)) == false)
+            if (AllowedImageExtensions.Split(ExtensionSeparator).Any(x => extension.EndsWith(x)) == false)
             {
-                throw new InvalidDataException($"Disallowed extension {extension}. Allowed extensions are {string.Join(", ", AllowedImageExtensions)}");
+                throw new InvalidDataException($"Disallowed extension {extension}. Allowed extensions are {AllowedImageExtensions.Replace(",", ", ")}");
             }
         }
     }

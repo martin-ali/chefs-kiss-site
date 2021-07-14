4f6122ea8e5bcfbdbf9fe4a4fbc27133ede34c19
Tectonik <marto7395@gmail.com>
2021-07-14 22:53:41 +0300

    Added author information to recipe details view and viewmodel

--- ChefsKiss/Web/ChefsKiss.Web/Areas/Recipes/ViewModels/Recipes/RecipeDetailsViewModel.cs
+++ ChefsKiss/Web/ChefsKiss.Web/Areas/Recipes/ViewModels/Recipes/RecipeDetailsViewModel.cs
@@ -11,6 +11,10 @@ namespace ChefsKiss.Web.Areas.Recipes.ViewModels.Recipes
 
     public class RecipeDetailsViewModel : RecipeInListViewModel, IMapFrom<Recipe>, IHaveCustomMappings
     {
+        public string AuthorId { get; init; }
+
+        public string AuthorUsername { get; init; }
+
         public string Content { get; init; }
 
         public IEnumerable<ReviewListViewModel> Reviews { get; init; } = new List<ReviewListViewModel>();

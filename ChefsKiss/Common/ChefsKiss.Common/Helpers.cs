8a7c3baac40c9247914d5b9868991ac11d40e437
Martin <marto7395@gmail.com>
2021-07-16 12:50:36 +0300

    Added GetReadableFileSize helper

--- ChefsKiss/Common/ChefsKiss.Common/Helpers.cs
+++ ChefsKiss/Common/ChefsKiss.Common/Helpers.cs
@@ -12,5 +12,23 @@ namespace ChefsKiss.Common
 
             return controllerName;
         }
+
+        public static string GetReadableFileSize(int bytes)
+        {
+            var data = bytes;
+            var units = new string[] { "B", "KB", "MB", "GB", "TB" };
+            var unitIndex = 0;
+
+            while (data > 1024)
+            {
+                data /= 1024;
+                unitIndex++;
+            }
+
+            var unit = units[unitIndex];
+            var size = $"{data}{unit}";
+
+            return size;
+        }
     }
 }

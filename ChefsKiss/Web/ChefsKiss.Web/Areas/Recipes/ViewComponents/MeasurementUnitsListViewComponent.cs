e6bea473e6dd54209899ca208ac82d0a71dd92b4
Martin <marto7395@gmail.com>
2021-07-31 11:44:49 +0300

    Added note to ViewComponent

--- ChefsKiss/Web/ChefsKiss.Web/Areas/Recipes/ViewComponents/MeasurementUnitsListViewComponent.cs
+++ ChefsKiss/Web/ChefsKiss.Web/Areas/Recipes/ViewComponents/MeasurementUnitsListViewComponent.cs
@@ -19,6 +19,7 @@
             this.measurementUnitsService = measurementUnitsService;
         }
 
+        // NOTE: The method being async was a requirement, not my own decision
         public async Task<IViewComponentResult> InvokeAsync(int selected = 0)
         {
             var items = this.measurementUnitsService.GetAll<MeasurementUnitViewModel>();

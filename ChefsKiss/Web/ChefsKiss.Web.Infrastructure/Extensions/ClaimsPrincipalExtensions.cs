62629f01e220b159a84886a5b942c9510772173c
Martin <marto7395@gmail.com>
2021-08-10 15:33:48 +0300

    Added ClaimsPrincipalExtensions

---
+++ ChefsKiss/Web/ChefsKiss.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+namespace ChefsKiss.Web.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    using static ChefsKiss.Common.WebConstants;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user) => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        public static bool IsAdmin(this ClaimsPrincipal user) => user.IsInRole(AdministratorRoleName);
+    }
+}

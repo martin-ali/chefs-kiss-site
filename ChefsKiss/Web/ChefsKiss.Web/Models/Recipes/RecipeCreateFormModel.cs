c075b7c75bd621756b3eeb1c2220cfd33a3c09bc
Martin <marto7395@gmail.com>
2021-08-22 12:09:32 +0300

    Added image  constraints to RecipeCreateFormModel

--- ChefsKiss/Web/ChefsKiss.Web/Models/Recipes/RecipeCreateFormModel.cs
+++ ChefsKiss/Web/ChefsKiss.Web/Models/Recipes/RecipeCreateFormModel.cs
@@ -2,11 +2,17 @@ namespace ChefsKiss.Web.Models.Recipes
 {
     using System.ComponentModel.DataAnnotations;
 
+    using ChefsKiss.Web.Infrastructure.Attributes;
+
     using Microsoft.AspNetCore.Http;
+
+    using static ChefsKiss.Common.DataConstants;
 
     public class RecipeCreateFormModel : RecipeFormModel
     {
         [Required]
+        [ImagesOnly]
+        [MaxFileSize(Images.MaxSizeBytes)]
         public override IFormFile Image { get; init; }
     }
 }
